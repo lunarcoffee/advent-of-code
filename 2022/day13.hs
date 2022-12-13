970dc069e079708851b811c976d9023bbcf9e306
lunarcoffee <lunarcoffee.pjc@gmail.com>
2022-12-13 16:02:16 -0500

    add day 13... i should really use parsec at this point

---
+++ 2022/day13.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TypeFamilies #-}
+
+import Control.Monad
+import Data.List (findIndices, sort)
+import Data.List.Split (chunksOf, splitOn)
+import Text.Regex.PCRE.Heavy (gsub, re)
+
+data Packet = Int Int | List [Packet] deriving (Eq, Read)
+
+instance Ord Packet where
+  compare (Int a) (Int b) = compare a b
+  compare (List a) (List b) = compare a b
+  compare a@(Int _) b = compare (List [a]) b
+  compare a b = compare a $ List [b]
+
+orderedIndexSum :: [Packet] -> Int
+orderedIndexSum = sum . map (+ 1) . findIndices (\[x, y] -> x < y) . chunksOf 2
+
+decoderKey :: [Packet] -> Int
+decoderKey =
+  let dividers = map (\x -> List [List [Int x]]) [2, 6]
+   in product . map (+ 1) . findIndices (`elem` dividers) . sort . (dividers ++)
+
+main :: IO ()
+main = do
+  packets <- parse <$> getContents
+  print $ orderedIndexSum packets
+  print $ decoderKey packets
+  where
+    prepend regex prefix = gsub regex $ (prefix ++) . head
+    parsePacket = read . prepend [re|(\d+)|] "Int " . prepend [re|(\[)|] "List "
+    parse = map parsePacket . lines <=< splitOn "\n\n"
