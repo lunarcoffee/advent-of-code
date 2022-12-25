85c8fc00c5b828bca267160670c86cdcc5efcfcc
lunarcoffee <lunarcoffee.pjc@gmail.com>
2022-12-25 14:00:38 -0500

    add day 22 (hardcoded for my cube net lol)

--- 2022/day22.hs
+++ 2022/day22.hs
@@ -1,11 +1,11 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE ViewPatterns #-}
 
+import Control.Applicative
 import Control.Lens hiding (re)
 import Control.Monad
-import Data.List (elemIndex, elemIndices, foldl', scanl')
-import Data.Maybe (isNothing)
+import Data.List (elemIndex, find, foldl', scanl')
+import Data.Maybe (fromJust, isJust)
 import Data.Vector (Vector, (!?))
 import Data.Vector qualified as Vec
 import Text.Regex.PCRE.Heavy (re, scan)
@@ -17,29 +17,56 @@ type Field = Vector (Vector (Maybe Char))
 (@) :: Field -> Pos -> Maybe Char
 cs @ (x, y) = join $ cs !? x >>= (!? y)
 
-runMove :: Field -> (Pos -> Pos) -> Pos -> Pos
-runMove cs m p@((cs @) . m -> Just c) = if c == '#' then p else m p
-runMove cs m p = runMove cs (wrap . m) p
-  where
-    wrap = head . dropWhile (isNothing . (cs @)) . iterate (join bimap (`mod` maxDim) . m)
-    maxDim = max (Vec.length cs) $ Vec.maximum $ Vec.length <$> cs
+fromFacing :: Int -> Pos -> Pos
+fromFacing = ((flip (+~) <$> [1, -1] <*> [_2, _1]) !!)
 
-flatPassword :: Field -> [Pos -> Pos] -> Int
-flatPassword cs ms =
+toFacing :: (Pos -> Pos) -> Maybe Int
+toFacing m = elemIndex (m (0, 0)) [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+type Wrapping = Field -> (Pos -> Pos) -> Pos -> (Pos, Int)
+
+stepMove :: Field -> Wrapping -> (Pos, Int) -> (Pos -> Pos) -> (Pos, Int)
+stepMove cs wrap s@(p, mo) m
+  | Just '#' <- cs @ m' p <|> cs @ p' = s
+  | Just _ <- cs @ m' p = s & _1 %~ m'
+  | otherwise = (p', (mo + mo') `mod` 4)
+  where
+    (p', mo') = wrap cs m' p
+    m' = maybe m (fromFacing . (`mod` 4) . (+ mo)) $ toFacing m
+
+wrapSide :: Wrapping
+wrapSide cs m = (,0) . fromJust . find (isJust . (cs @)) . iterate wrapMove . m
+  where
+    wrapMove = (both %~ (`mod` 200)) . m
+
+wrapCube :: Wrapping
+wrapCube _ m p@(r, c)
+  | fst << 49 = ifFacing 0 ((149 - r, 99), 2) ((149 - r, 0), 2)
+  | fst << 99 = ifFacing 0 ((49, r + 50), 3) ((100, r - 50), 3)
+  | fst << 149 = ifFacing 0 ((149 - r, 149), 2) ((149 - r, 50), 2)
+  | fst << 199 = ifFacing 0 ((149, r - 100), 3) ((0, r - 100), 3)
+  | snd << 49 = ifFacing 1 ((0, c + 100), 0) ((c + 50, 50), 1)
+  | snd << 99 = ifFacing 1 ((c + 100, 49), 1) ((c + 100, 0), 1)
+  | otherwise = ifFacing 1 ((c - 50, 99), 1) ((199, c - 100), 0)
+  where
+    ifFacing f a b = if toFacing m == Just f then a else b
+    dim << top = all ((`elem` [top - 49 .. top]) . dim) [p, m p]
+
+findPassword :: Field -> [Pos -> Pos] -> Wrapping -> Int
+findPassword cs ms wrap =
   let Just start = Vec.elemIndex (Just '.') $ Vec.head cs
-      (row, col) = foldl' (flip $ runMove cs) (0, start) ms
-      Just facing = elemIndex (last ms (0, 0)) [(0, 1), (1, 0), (0, -1), (-1, 0)]
+      ((row, col), mo) = foldl' (stepMove cs wrap) ((0, start), 0) ms
+      facing = (fromJust (toFacing $ last ms) + mo) `mod` 4
    in 1_000 * (row + 1) + 4 * (col + 1) + facing
 
 main :: IO ()
 main = do
   (cells, moves) <- bimap parseMap (parseMoves . last) . span (/= "") . lines <$> getContents
-  print $ flatPassword cells moves
+  print $ findPassword cells moves wrapSide
+  print $ findPassword cells moves wrapCube
   where
+    parseMap = Vec.fromList . map (Vec.fromList . map ((<$) <*> guard . (/= ' ')))
     parseMoves = fst <=< scanl' parsePair ([], 0) . map snd . scan [re|(\d+)(\w)?|]
     parsePair s [n] = parsePair s [n, "F"]
-    parsePair (_, d) [n, [dir]] =
-      let dirs = flip (+~) <$> [1, -1] <*> [_2, _1]
-       in (replicate (read n) (dirs !! d), (d + head (elemIndices dir "LFR") - 1) `mod` 4)
-    parseMap = Vec.fromList . map (Vec.fromList . map parseCell)
-    parseCell c = c <$ guard (c /= ' ')
+    parsePair (_, d) [n, [dir]] = (replicate (read n) $ fromFacing d, (d + parseDir dir) `mod` 4)
+    parseDir = pred . fromJust . (`elemIndex` "LFR")
